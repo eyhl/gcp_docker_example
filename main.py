c2456a6611a75f71e051c97c99d5adea5914c75a
eyhl <eyhli@dtu.dk>
2022-01-11 14:15:20 +0100

    added docstring and changed gamma to 2e-3

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+'''
+Yo this script is missing a docstring!
+'''
+
 from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
 
@@ -10,7 +14,7 @@ if __name__ == '__main__':
     data = digits.images.reshape((n_samples, -1))
 
     # Create a classifier: a support vector classifier
-    clf = svm.SVC(gamma=0.001)
+    clf = svm.SVC(gamma=0.002)
 
     # Split data into 50% train and 50% test subsets
     X_train, X_test, y_train, y_test = train_test_split(
